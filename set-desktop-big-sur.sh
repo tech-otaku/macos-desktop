af35ae7ff85f3a14e321faef292506b7d520187a
Steve Ward <tech-otaku@users.noreply.github.com>
2020-11-05 11:42:59 +0000

    Update Image Options
    Removed:
    'mojave', 'mojave-light' and 'mojave-dark'
    
    Added:
    'beach', 'beach-light' and 'beach-dark'
    'cliffs', 'cliffs-light' and 'cliffs-dark'
    'desert', 'desert-light' and 'desert-dark'
    'dome', 'dome-light' and 'dome-dark'
    'iridescence', 'iridescence-light' and 'iridescence-dark'
    'lake', 'lake-light' and 'lake-dark'
    'peak', 'peak-light' and 'peak-dark'
    'tree', 'tree-light' and 'tree-dark'
    'valley', 'valley-light' and 'valley-dark'

--- set-desktop-big-sur.sh
+++ set-desktop-big-sur.sh
@@ -11,22 +11,46 @@
             # graphic = Big Sur Graphic Automatic
             # light = Big Sur Graphic Light (Still)
             # dark = Big Sur Graphic Dark (Still)
+            # beach = The Beach Dynamic
+            # beach-light = The Beach Light (Still)
+            # beach-dark = The Beach Dark (Still)
             # big-sur = Big Sur Dynamic
             # big-sur-light = Big Sur Light (Still)
             # big-sur-dark = Big Sur Dark (Still)
             # catalina = Catalina Dynamic
             # catalina-light = Catalina Light (Still)
             # catalina-dark = Catalina Dark (Still)
-            # mojave = Mojave Dynamic
-            # mojave-light = Mojave Light (Still)
-            # mojave-dark = Mojave Dark (Still)
+            # cliffs = The Cliffs Dynamic
+            # cliffs-light = The Cliffs Light (Still)
+            # cliffs-dark = The Cliffs Dark (Still)
+            # desert = The Desert Dynamic
+            # desert-light = The Desert Light (Still)
+            # desert-dark = The Desert Dark (Still)
+            # iridescence = Iridescence Automatic
+            # iridescence-light = Iridescence Light (Still)
+            # iridescence-dark = Iridescence Dark (Still)
+            # dome = Dome Automatic
+            # dome-light = Dome Light (Still)
+            # dome-dark = Dome Dark (Still)
+            # lake = The Lake Dynamic
+            # lake-light = The Lake Light (Still)
+            # lake-dark = The Lake Dark (Still)
+            # peak = Peak Automatic
+            # peak-light = Peak Light (Still)
+            # peak-dark = Peak Dark (Still)
             # solar = Solar Gradients
+            # tree = Tree Automatic
+            # tree-light = Tree Light (Still)
+            # tree-dark = Tree Dark (Still)
+            # valley = Valley Automatic
+            # valley-light = Valley Light (Still)
+            # valley-dark = Valley Dark (Still)
         # non-HEIF (.heic) images e.g.
-            # "/System/Library/Desktop Pictures/High Sierra.jpg"
+            # "/System/Library/Desktop Pictures/Reflection 1.jpg"
         # Other
             # default = set the database [$db] to the default
 
-# EXAMPLE: [bash] ./set-desktop-big-sur.sh light
+# EXAMPLE: [bash] ./set-desktop-big-sur.sh beach
 
 
 
@@ -63,7 +87,7 @@
 
     db="${HOME}"/Library/Application\ Support/Dock/desktoppicture.db
     backup="${HOME}"/Library/Application\ Support/Dock/backup
-    options=("big-sur" "big-sur-light" "big-sur-dark" "catalina" "catalina-light" "catalina-dark" "dark"  "default" "graphic" "light" "mojave" "mojave-light" "mojave-dark" "solar")
+    options=("beach" "beach-light" "beach-dark" "big-sur" "big-sur-light" "big-sur-dark" "catalina" "catalina-light" "catalina-dark" "cliffs" "cliffs-light" "cliffs-dark" "dark"  "default" "desert" "desert-light" "desert-dark" "dome" "dome-light" "dome-dark" "graphic" "iridescence" "iridescence-light" "iridescence-dark" "lake" "lake-light" "lake-dark" "light" "peak" "peak-light" "peak-dark" "tree" "tree-light" "tree-dark" "valley" "valley-light" "valley-dark" "solar")
 
 
 
@@ -147,6 +171,21 @@
             value=3
             image="Big Sur Graphic Dark (Still)"
             ;;
+		beach)
+            value=1
+            image="The Beach Dynamic"
+            file="'/System/Library/Desktop Pictures/The Beach.heic'"
+            ;;
+        beach-light)
+            value=2
+            image="The Beach Light (Still)"
+            file="'/System/Library/Desktop Pictures/The Beach.heic'"
+            ;;
+        beach-dark)
+            value=3
+            image="The Beach Dark (Still)"
+            file="'/System/Library/Desktop Pictures/The Beach.heic'"
+            ;;
         big-sur)
             value=1
             image="Big Sur Dynamic"
@@ -177,6 +216,81 @@
             image="Catalina Dark (Still)"
             file="'/System/Library/Desktop Pictures/Catalina.heic'"
             ;;
+		cliffs)
+            value=1
+            image="The Cliffs Dynamic"
+            file="'/System/Library/Desktop Pictures/The Cliffs.heic'"
+            ;;
+        cliffs-light)
+            value=2
+            image="The Cliffs Light (Still)"
+            file="'/System/Library/Desktop Pictures/The Cliffs.heic'"
+            ;;
+        cliffs-dark)
+            value=3
+            image="The Cliffs Dark (Still)"
+            file="'/System/Library/Desktop Pictures/The Cliffs.heic'"
+            ;;
+		desert)
+            value=1
+            image="The Desert Dynamic"
+            file="'/System/Library/Desktop Pictures/The Desert.heic'"
+            ;;
+        desert-light)
+            value=2
+            image="The Desert Light (Still)"
+            file="'/System/Library/Desktop Pictures/The Desert.heic'"
+            ;;
+        desert-dark)
+            value=3
+            image="The Desert Dark (Still)"
+            file="'/System/Library/Desktop Pictures/The Desert.heic'"
+            ;;
+		dome)
+            value=1
+            image="Dome Automatic"
+            file="'/System/Library/Desktop Pictures/Dome.heic'"
+            ;;
+        dome-light)
+            value=2
+            image="Dome Light (Still)"
+            file="'/System/Library/Desktop Pictures/Dome.heic'"
+            ;;
+        dome-dark)
+            value=3
+            image="Dome Dark (Still)"
+            file="'/System/Library/Desktop Pictures/Dome.heic'"
+            ;;
+		iridescence)
+            value=1
+            image="Iridescence Automatic"
+            file="'/System/Library/Desktop Pictures/Iridescence.heic'"
+            ;;
+        iridescence-light)
+            value=2
+            image="Iridescence Light (Still)"
+            file="'/System/Library/Desktop Pictures/Iridescence.heic'"
+            ;;
+        iridescence-dark)
+            value=3
+            image="Iridescence Dark (Still)"
+            file="'/System/Library/Desktop Pictures/Iridescence.heic'"
+            ;;
+		lake)
+            value=1
+            image="The Lake Dynamic"
+            file="'/System/Library/Desktop Pictures/The Lake.heic'"
+            ;;
+        lake-light)
+            value=2
+            image="The Lake Light (Still)"
+            file="'/System/Library/Desktop Pictures/The Lake.heic'"
+            ;;
+        lake-dark)
+            value=3
+            image="The Lake Dark (Still)"
+            file="'/System/Library/Desktop Pictures/The Lake.heic'"
+            ;;
         mojave)
             value=1
             image="Mojave Dynamic"
@@ -192,10 +306,55 @@
             image="Mojave Dark (Still)"
             file="'/System/Library/Desktop Pictures/Mojave.heic'"
             ;;
+		peak)
+            value=1
+            image="Peak Automatic"
+            file="'/System/Library/Desktop Pictures/Peak.heic'"
+            ;;
+        peak-light)
+            value=2
+            image="Peak Light (Still)"
+            file="'/System/Library/Desktop Pictures/Peak.heic'"
+            ;;
+        peak-dark)
+            value=3
+            image="Peak Dark (Still)"
+            file="'/System/Library/Desktop Pictures/Peak.heic'"
+            ;;
         solar)
             value=1
             image="Solar Gradients"
             file="'/System/Library/Desktop Pictures/Solar Gradients.heic'"
+            ;;
+		tree)
+            value=1
+            image="Tree Automatic"
+            file="'/System/Library/Desktop Pictures/Tree.heic'"
+            ;;
+        tree-light)
+            value=2
+            image="Tree Light (Still)"
+            file="'/System/Library/Desktop Pictures/Tree.heic'"
+            ;;
+        tree-dark)
+            value=3
+            image="Tree Dark (Still)"
+            file="'/System/Library/Desktop Pictures/Tree.heic'"
+            ;;
+		valley)
+            value=1
+            image="Valley Automatic"
+            file="'/System/Library/Desktop Pictures/Valley.heic'"
+            ;;
+        valley-light)
+            value=2
+            image="Valley Light (Still)"
+            file="'/System/Library/Desktop Pictures/Valley.heic'"
+            ;;
+        valley-dark)
+            value=3
+            image="Valley Dark (Still)"
+            file="'/System/Library/Desktop Pictures/Valley.heic'"
             ;;
         default)
             # Set the database [$db] to the default and exit. Only works if $db is ~/Library/Application\ Support/Dock/desktoppicture.db
@@ -264,9 +423,19 @@
     sqlite3 "${db}" "INSERT INTO preferences(rowid,key,data_id,picture_id) VALUES( $((lastrow[1] + 4)),${key},${data_id},1);"
 
 
-# Additional rows need to be inserted into the `data` and `preferences` table if the desktop image is being set to Big Sur Dynamic, Big Sur Light (Still), Big Sur Dark (Still),
-# Catalina Dynamic, Catalina Light (Still), Catalina Dark (Still), Mojave Dynamic, Mojave Light (Still) or Mojave Dark (Still)
-    if [[ "${1}" == big-sur* ]] || [[ "${1}" == catalina* ]] || [[ "${1}" == mojave* ]] || [[ "${1}" == solar ]]; then
+# Additional rows need to be inserted into the `data` and `preferences` table if the desktop image is being set to 
+# Beach Dynamic, Beach Light (Still) or Beach Dark (Still)
+# Big Sur Dynamic, Big Sur Light (Still) or Big Sur Dark (Still)
+# Catalina Dynamic, Catalina Light (Still) or Catalina Dark (Still)
+# The Cliffs Dynamic, The Cliffs Light (Still) or The Cliffs Dark (Still)
+# The Desert Dynamic, The Desert Light (Still) or The Desert Dark (Still)
+# Dome Automatic, Dome Light (Still) or Dome Dark (Still)
+# Iridescence Automatic, Iridescence Light (Still) or Iridescence Dark (Still)
+# The Lake Dynamic, The Lake Light (Still) or The Lake Dark (Still)
+# Peak Automatic, Peak Light (Still) or Peak (Still)
+# Tree Automatic, Tree Light (Still) or Tree (Still)
+# Valley Automatic, Valley Light (Still) or Valley (Still)
+    if [[ "${1}" == beach* ]] || [[ "${1}" == big-sur* ]] || [[ "${1}" == catalina* ]] || [[ "${1}" == cliffs* ]] || [[ "${1}" == desert* ]] || [[ "${1}" == dome* ]] || [[ "${1}" == iridescence* ]] || [[ "${1}" == lake* ]] || [[ "${1}" == peak* ]] || [[ "${1}" == solar ]] || [[ "${1}" == tree* ]] || [[ "${1}" == valley* ]]; then
 
     # Insert a new row into the `data` table.
         sqlite3 "${db}" "INSERT INTO data(rowid,value) VALUES( $((lastrow[0] + 2)), ${file} );"
