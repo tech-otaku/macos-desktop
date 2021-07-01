bb9248a4a24e61250fdc8c78448db8b17d7bb212
Steve <steve@tech-otaku.com>
2021-07-01 08:39:23 +0100

    Create Script to Randomly Update the Desktop Picture
    Executes the set-desktop.sh script a given number of times, each time
    passing it an option or filename randomly selected.

---
+++ Monterey Tests/set-desktop-monterey-tests.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+[ -z $1 ] && TIMES=5 || TIMES=$1
+
+rm set-desktop-monterey-tests.txt
+
+echo -e "Randomly generated $(date '+on %Y-%m-%d at %H:%M:%S') by $0 using Monterey Developer Beta 1 build $(system_profiler SPSoftwareDataType | awk '/System Version/ {print $5}')\n" >> set-desktop-monterey-tests.txt 
+
+pushd ../ > /dev/null
+bash set-desktop.sh default
+popd > /dev/null
+
+sleep 3
+
+for (( i=1;i<=$TIMES;i++ )); do 
+	RANDNUM=$((1 + RANDOM % $(wc -l <  desktop-images.txt)))
+	SELECTION=$(sed ''$RANDNUM'!d' desktop-images.txt)
+
+    pushd ../ > /dev/null
+	bash set-desktop.sh "$SELECTION"
+    popd > /dev/null
+	
+	sleep 3
+
+	echo -e "$i/$TIMES. $SELECTION:\n" >> set-desktop-monterey-tests.txt
+	echo -e "--------------------------------- data ---------------------------------  --------- preferences ---------" >> set-desktop-monterey-tests.txt
+	sqlite3 ${HOME}/Library/Application\ Support/Dock/desktoppicture.db < execute.sql >> set-desktop-monterey-tests.txt
+	echo -e "\n\n" >> set-desktop-monterey-tests.txt
+done
